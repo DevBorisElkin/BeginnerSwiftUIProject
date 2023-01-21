fc21e9c671037585a27302ea20f248d92360be5f
DevBorisElkin <ea.foreverrr@gmail.com>
2023-01-21 20:11:10 +0300

    Initial commit

---
+++ BeginnerSwiftUIProject/BeginnerSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginnerSwiftUIProjectApp.swift
+//  BeginnerSwiftUIProject
+//
+//  Created by test on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BeginnerSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
